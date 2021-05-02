7b717242490e290451ae99cdfa952caff3ae3f7b
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-05-02 03:51:06 -0400

    Update the Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Footer from './components/Footer'
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Footer/>
+      <Header />
+      <h1>deeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</h1>
+      <h1>ddddddddddecededededee</h1>
+      <Footer />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,2 +1,27 @@
-import React from 'react';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { MenuIcon } from "@material-ui/icons";
 
+const Header = (props) => {
+  return (
+    <header>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">News</Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
+};
+
+export default Header;
